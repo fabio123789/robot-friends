c2ffba300da237e20f32db8759fd6201d422b106
fabio123789 <goncalvesfabio227@gmail.com>
2024-05-06 18:10:52 +0100

    Updates

--- src/components/scroll/Scroll.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Scroll = ({children}) => {
-    return <div style={{overflow: 'scroll', height: '100vh', width: '100%', borderTop: '5px solid black'}}>
-    {children}
-    </div> 
-}
-
-export default Scroll
